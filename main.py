f6be94a3befd62a1b97774557c01395cc17c6d4f
Gertobin11 <gertobin11@gmail.com>
2022-07-27 15:31:42 +0100

    Refactored the snake
    Created a snake class and moved it into its own file where an instance of it can now be created in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from time import sleep
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,40 +10,21 @@ screen.tracer(0)
 game_is_on = True
 snake = []
 
-def create_snake():
-    n = 0
-    for i in range(0, 3):
-        snake_body = Turtle('square')
-        snake_body.color('black', 'white')
-        snake_body.setx(n)
-        n -= 20
-        snake.append(snake_body)
+snake = Snake()
+snake.create()
 
-create_snake()
 screen.update()
-snake_head = snake[0]
-
-def turn_left():
-    snake_head.left(90)
-
-def turn_right():
-    snake_head.right(90)
+snake_head = snake.get_snake_head()
 
 screen.listen()
-screen.onkey(key='a', fun=turn_left)
-screen.onkey(key='d', fun=turn_right)
+screen.onkey(key='a', fun=snake.turn_left)
+screen.onkey(key='d', fun=snake.turn_right)
 
 
 while game_is_on:
     sleep(0.3)
     screen.update()
-    for seg_num in range(len(snake) - 1, 0, -1):
-        cords = snake[seg_num - 1].pos()
-        snake[seg_num].goto(cords)
-    snake_head.forward(20)
+    snake.move()
 
 
-screen.listen()
-screen.onkey(key='w', fun=turn_left)
-    
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+class Snake():
+
+    def __init__(self):
+        self.body = []
+
+    def create(self):
+        n = 0
+        for i in range(0, 3):
+            snake_body = Turtle('square')
+            snake_body.color('black', 'white')
+            snake_body.setx(n)
+            n -= 20
+            self.body.append(snake_body)
+
+    def get_snake_head(self):
+        return self.body[0]
+
+    def move(self):
+        for seg_num in range(len(self.body) - 1, 0, -1):
+            cords = self.body[seg_num - 1].pos()
+            self.body[seg_num].goto(cords)
+        self.get_snake_head().forward(20)
+
+    def turn_left(self):
+        self.get_snake_head().left(90)
+
+    def turn_right(self):
+        self.get_snake_head().right(90)
